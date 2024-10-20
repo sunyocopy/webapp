1c1537264d3876476f0782e7ec873fa07fc54a18
sunyocopy <127908488+sunyocopy@users.noreply.github.com>
2024-10-20 21:54:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+const day = document.getElementById("day");
 
+var monthName = [
+  "มกราคม",
+  "กุมภาพันธ์",
+  "มีนาคม",
+  "เมษายน",
+  "พฤษภาคม",
+  "มิถุนายน",
+  "กรกฎาคม",
+  "สิงหาคม",
+  "กัยยายน",
+  "ตุลาคม",
+  "พฤศจิกายน",
+  "ธันวาคม"
+];
+
+const clock = setInterval(function time() {
+  let today = new Date();
+  let d = today.getDate();
+  let m = today.getMonth();
+  let y = today.getFullYear();
+  let h = today.getHours();
+  let min = today.getMinutes();
+  let sec = today.getSeconds();
+
+  day.innerHTML = `วันที่ ${d} ${monthName[m]} ${y + 543}`;
+  hour.textContent = h;
+  minute.innerText = min;
+  second.innerText = sec;
+});
